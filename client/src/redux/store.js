2fd03aa6d3c6abb1aa8ebeab0cb2fea9ca037a34
enriqueMontano <montanoenrique@outlook.com>
2020-08-29 09:59:53 +0200

    Setup reducer

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,10 +1,23 @@
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 
-const initialState = [];
+const initialState = {
+  pending: false,
+  phones: [],
+  error: null,
+};
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
+    case 'FETCH_PHONES_PENDING':
+      return { ...state, pending: true };
+
+    case 'FETCH_PHONES_SUCCESS':
+      return { ...state, pending: false, phones: action.payload };
+
+    case 'FETCH_PHONES_ERROR':
+      return { ...state, pending: false, error: action.error };
+
     default:
       return state;
   }
